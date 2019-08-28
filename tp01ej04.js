bd8b26f5fb65c76259a1db639b99ff9934d88a51
Conrado Chiesa <conradochiesa@gmail.com>
2019-08-28 17:58:03 -0300

    Acomodados en clase

--- tp01ej04.js
+++ tp01ej04.js
@@ -1,12 +1,32 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let h = 300;
+var degrade = ctx.createImageData(h, h);
 
-var degrade = ctx.createLinearGradient(0,0,0,300);
-degrade.addColorStop(0, "#000000");
-degrade.addColorStop(1, "#FFFFFF");
-ctx.fillStyle = degrade;
-ctx.fillRect(50, 50, 250, 350);
-ctx.strokeStyle = "#00FF00";
-ctx.lineWidth = 5;
-ctx.strokeRect(50, 50, 250, 350);
 
+
+for (let i = 0; i < degrade.width; i++) {
+    for (let j = 0; j < degrade.height; j++) {
+        let color = (j/h)*255;
+        setPixel(degrade, i, j, color);
+    }
+}
+ctx.putImageData(degrade, 0, 0);
+function setPixel(degrade, i, j, color) {
+    let index = (i + j * degrade.width) * 4;
+    degrade.data[index] = color;
+    degrade.data[index+1] = color;
+    degrade.data[index+2] = color;
+    degrade.data[index+3] = 255;
+}
+
+// let degrade = ctx.createLinearGradient(0,0,0,300);
+// degrade.addColorStop(0, "#000000");
+// degrade.addColorStop(1, "#FFFFFF");
+// ctx.fillStyle = degrade;
+// ctx.fillRect(50, 50, 250, 350);
+// ctx.strokeStyle = "#00FF00";
+// ctx.lineWidth = 5;
+// ctx.strokeRect(50, 50, 250, 350);
+
+// color = (j/h)*255 donde J es el segundo for y H el alto de la imagen
--- tp01ej06.js
+++ tp01ej06.js
@@ -1,6 +1,7 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let width = canvas.width;
+let height = canvas.height;
 
 let img01 = new Image();
 let imgData = img01.data;
@@ -8,18 +9,24 @@ img01.src = "/home/konrad/Documentos/Interfaces/Aguila.jpg";
 img01.onload = function() {
     ctx.drawImage(img01, 0, 0);
 }
-let imgToGray = ctx.getImageData(0, 0, canvas.Width, canvas.height);
-let data1 = imgToGray.data;
+apliFilter(img01);
 
-for (let i = 0; i < data1.length; i++) {
-    let gris = parseInt((data1[i] + data1[i + 1] + data1[i + 2]) / 3);
-    data1[i] = gris;
-    data1[i + 1] = gris;
-    data1[i + 2] = gris;
-}
+function apliFilter(img01){
 
-img01.onload = function() {
-    ctx.putImageData(imgToGray,0,0);
-
-    ctx.drawImage(img01, 0, 0);
+    var imgToGray = ctx.getImageData(0, 0, width, height);
+    let data1 = imgToGray.data;
+    
+    for (let i = 0; i < data1.length; i += 4) {
+        let gris = parseInt((data1[i] + data1[i + 1] + data1[i + 2]) / 3);
+        data1[i] = gris;
+        data1[i + 1] = gris;
+        data1[i + 2] = gris;
+        data1[i + 3] = 255;
+    }
+    
+    img01.onload = function() {
+        //ctx.drawImage(img01, 0, 0);
+        //ctx.putImageData(imgToGray,0,0);
+        
+    }
 }
