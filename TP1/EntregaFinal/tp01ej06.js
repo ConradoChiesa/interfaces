d4191790e526466c71dd1ff2ff879e6987b3145b
Conrado Chiesa <conradochiesa@gmail.com>
2019-09-02 17:11:06 -0300

    Faltan permisos Ejercicio 6

--- TP1/EntregaFinal/tp01ej06.js
+++ TP1/EntregaFinal/tp01ej06.js
@@ -1,35 +1,46 @@
+document.addEventListener('DOMContentLoaded', load());
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
-let canvas2 = document.getElementById('canvas2');
-let ctx2 = canvas.getContext('2d');
 let width = canvas.width;
 let height = canvas.height;
 
-let img01 = new Image();
-let imgData = img01.data;
-img01.src = "http://c2.peakpx.com/wallpaper/287/538/402/5k-art-face-spraypaint-wrinkle-wallpaper-preview.jpg";
-img01.onload = function() {
-    ctx.drawImage(img01, 0, 0);
+
+
+function load() {
     
-    apliFilter(img01);
+    let img01 = new Image();
+    let btn = document.getElementById("filtro01");
+
+    btn.addEventListener("click", function() {
+        apliFilter();
+    });
+
+    img01.src = "einstein.jpeg";
+    img01.onload = function() {
+        ctx.drawImage(img01, 0, 0);
+    }
 }
 
-function apliFilter(img01){
-    alert("va");
-    let imgToGray = ctx.getImageData(0, 0, img01.width, img01.height);
-    let data1 = imgToGray.data;
-    
-    for (let i = 0; i < imgToGray.width; i ++) {
-        for (let j = 0; j < imgToGray.height; j++) {
-                        
-            let r = getRed(imgToGray, i, j);
-            let g = getGreen(imgToGray, i, j);
-            let b = getBlue(imgToGray, i, j);
-            setPixel(imgToGray, i, j, r, g, b);
-
+function apliFilter(){
+    let img = new Image();
+    let imgData;
+    img.src = "einstein.jpeg";
+    img.onload = function() {
+        ctx.drawImage(this, 0, 0);
+        imgData = ctx.getImageData(0, 0, this.width, this.height);
+        for (let i = 0; i < img.width; i ++) {
+            for (let j = 0; j < img.height; j++) {
+                
+                let r = getRed(imgData, i, j);
+                let g = getGreen(imgData, i, j);
+                let b = getBlue(imgData, i, j);
+                setPixel(imgData, i, j, r, g, b);
+                
+            }
         }
-    }
-
+        ctx.putImageData(imgData,0,0);
+        
+    }    
     function setPixel(imgToGray, x, y, r, g, b) {
         let i = (x + y * imgToGray.width) * 4;
         let gris = (r + g + b) / 3;
@@ -52,8 +63,5 @@ function apliFilter(img01){
         let index = (x + y * imgToGray.width) * 4;
         return imgToGray.data[index+2];
     }
-
-    ctx.putImageData(imgToGray,0,0);
-    // ctx.drawImage(imgToGray, 0, 0);
 
 }
