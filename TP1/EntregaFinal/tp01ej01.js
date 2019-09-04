ddbbdd47978d51b8932bfb8b08b78341a0805f28
Conrado Chiesa <conradochiesa@gmail.com>
2019-09-03 21:05:16 -0300

    Faltaba completar el Ejercicio 1

--- TP1/EntregaFinal/tp01ej01.js
+++ TP1/EntregaFinal/tp01ej01.js
@@ -1,33 +1,30 @@
-document.addEventListener('DOMContentLoaded', load());
-
-function load() {
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-
+document.addEventListener('DOMContentLoaded', () => {
+    let w = 100;
+    let h = 100;
+    let canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
     ctx.font = "bold 22px sans-serif";
     ctx.fillText("Resultados en consola",20,20);
     let matriz = [];
-    let w = 10;
-    let h = 10;
+
     for(let i = 0; i < w; i++) {
         matriz[i] = [];
         for(let j = 0; j < h; j++){
             matriz[i][j] = Math.floor(Math.random() * 1001);
         }
     }
-console.table(matriz);
-getMax(matriz);
-getMaxParMinInpar(matriz);
-}   
+    console.table(matriz);
+    getMax(matriz);
+    getMaxParMinInpar(matriz);
+    getPromedios(matriz);
+});   
 
 function getMax(matriz) {
     let aux = 0;
     for (let i = 0; i < matriz.length; i++) {
         for (let j = 0; j < matriz.length; j++) {
-            if (aux < matriz[i][j]) {
+            if (aux < matriz[i][j])
                 aux = matriz[i][j];
-            }
-            
         }
     }
     console.log("El número más alto de la matriz es " + aux);
@@ -52,3 +49,21 @@ function getMaxParMinInpar(matriz) {
     console.log("El mínimo encontrado en las filas impares fue " + min);
 }
 
+function getPromedios(matriz) {
+    let promedio = [];
+    let aux;
+    let prom;
+    // console.table(promedio);
+    for (let i = 0; i < matriz.length; i++) {
+        aux = 0;
+        for (let j = 0; j < matriz.length; j++) {
+            // aux = aux + matriz[i][j];
+            aux += matriz[i][j];
+        }
+        // console.log(matriz[i][j]);
+        
+        prom = aux/matriz.length;
+        promedio.push(prom);
+    }
+    console.table(promedio);
+}
