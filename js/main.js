450b976719151d2e10ef28fbc54e72ce5af757c6
Conrado Chiesa <conradochiesa@gmail.com>
2019-09-23 00:01:09 -0300

    Version entregable con varias mejoras en la view.

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ let boy = document.getElementById('boy');
 let coin = document.getElementById('coin');
 let coins = new Number();
 document.addEventListener("DOMContentLoaded", () => {
-
-document.addEventListener("keydown", handleKeyDown);
+    // startView(); // hacer
+document.addEventListener("keydown", handleKeyDown); // sacar
 });
 
 function handleKeyDown(e) {
@@ -37,14 +37,26 @@ function detectarColision(){
         if(a_pos.l <= b_pos.r && a_pos.r >= b_pos.l 
         && a_pos.b >= b_pos.t && a_pos.t <= b_pos.b ) {
                 colision()
+                playSound()
         }
     }, 300);
     
 }
 
+function playSound() {
+    let audio = {};
+    audio["bien"] = new Audio();
+    // audio["bien"].src = "./css/sound/Bienboludo.wma"
+    // audio["bien"].addEventListener('load', () => {
+        audio["bien"].play();
+        
+// });
+}
 function colision() {
     coins += 1;
     console.log(coins);
     coin.className = "hidden";
-    
+    setTimeout( () => {
+    coin.className = "coin";
+    }, 900);
 }
