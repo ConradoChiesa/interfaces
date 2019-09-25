d2d9b222fbcd42e095ad74360fa9ff7f7ca4b07f
Conrado Chiesa <conradochiesa@gmail.com>
2019-09-25 08:29:46 -0300

    Bastante avanzado. Falta arreglar fondos

--- js/main.js
+++ js/main.js
@@ -1,22 +1,52 @@
 let boy = document.getElementById('boy');
 let coin = document.getElementById('coin');
 let coins = new Number();
-document.addEventListener("DOMContentLoaded", () => {
-    // startView(); // hacer
-document.addEventListener("keydown", handleKeyDown); // sacar
-});
+let timer = new Number();
+let startView = document.getElementById('startView');
+let time = document.getElementById('time');
+let score = document.getElementById('score');
+document.addEventListener("DOMContentLoaded", start);
+
+function start() {
+    let btnPlay = document.getElementById("play");
+    let btnExit = document.getElementById("exit");
+    btnPlay.addEventListener("click", startGame);
+}
+
+function startGame() {
+    timer = 5;
+    document.addEventListener("keydown", handleKeyDown);
+    startView.className = "hidden";
+    var countDown = setInterval( () => {
+        let txtTime = "Tiempo: " + timer;
+        let txtScore =  "Monedas: " + coins;
+        time.innerHTML = txtTime;
+        score.innerHTML = txtScore;
+        if (timer > 0) {
+            timer--;
+            console.log(timer);
+        } else {
+            finishGame();
+            clearInterval(countDown);
+        }
+    }, 1000);
+}
+
+function finishGame() {
+    console.log("finish");
+    document.removeEventListener("keydown", handleKeyDown);
+    startView.className = "startView";
+}
 
 function handleKeyDown(e) {
     if (e.keyCode == 38) {
-            boy.className = "boyJump";
-            
-            setTimeout( () => { 
-                boy.className = "boyRun";
-            }, 600);
-            detectarColision()
-        }
-
+        boy.className = "boyJump";
+        setTimeout( () => { 
+            boy.className = "boyRun";
+        }, 600);
+        detectarColision()
     }
+}
 
 function detectarColision(){
     setTimeout( () => {
@@ -32,31 +62,27 @@ function detectarColision(){
                 r: coin.getBoundingClientRect().left + coin.getBoundingClientRect().width, 
                 b: coin.getBoundingClientRect().top + coin.getBoundingClientRect().height
             };
-    
-        //Detecta si se superponen las áreas
         if(a_pos.l <= b_pos.r && a_pos.r >= b_pos.l 
         && a_pos.b >= b_pos.t && a_pos.t <= b_pos.b ) {
                 colision()
                 playSound()
         }
-    }, 300);
-    
+    }, 300);   
 }
 
 function playSound() {
     let audio = {};
     audio["bien"] = new Audio();
-    // audio["bien"].src = "./css/sound/Bienboludo.wma"
-    // audio["bien"].addEventListener('load', () => {
-        audio["bien"].play();
-        
-// });
+    audio["bien"].src = "js/sound/buenaViejo.mp4";
+    audio["bien"].play();
 }
+
 function colision() {
     coins += 1;
+    timer = timer + 3;
     console.log(coins);
     coin.className = "hidden";
     setTimeout( () => {
-    coin.className = "coin";
+        coin.className = "coin";
     }, 900);
 }
