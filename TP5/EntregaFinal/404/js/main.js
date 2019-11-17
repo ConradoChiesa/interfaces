c4d31aedb4526e547e08cc8e0cca4bf5eeaa317f
ConradoChiesa <cchiesa@alumnos.exa.unicen.edu.ar>
2019-11-17 11:01:28 -0300

    Avances templeta planes falta boton

---
+++ TP5/EntregaFinal/404/js/main.js
@@ -0,0 +1,148 @@
+document.addEventListener("DOMContentLoaded", start);
+const gameTime = 30;
+let coins = new Number();
+let remainingTime = new Number();
+let boy = document.getElementById('boy');
+let coin = document.getElementById('coin');
+let coinContainer = document.getElementById('coin-container');
+let startView = document.getElementById('startView');
+let time = document.getElementById('time');
+let score = document.getElementById('score');
+let back = document.getElementById('back');
+let floor = document.getElementById('floor');
+let finish = true;
+let jumping = false;
+let firstPosY = boy.getBoundingClientRect().top;
+let now, dt, last, startJump, coinManegerTime = timestamp();
+let raf; // requestAnimationFrame
+
+
+function start() {
+    coin.className = "hidden";
+    coinContainer.className = "hidden";
+    let btnPlay = document.getElementById("play");
+    let btnExit = document.getElementById("exit");
+    btnPlay.addEventListener("click", startGame);
+    btnExit.addEventListener("click", handleExitGame)
+}
+
+function update() {
+    if (remainingTime > 0) {
+        remainingTime--;
+        detectarColision()
+    } else {
+        finish = true;
+    }
+    if (boy.getBoundingClientRect().top != firstPosY && timestamp() > startJump + 600)
+    jumping = false;
+    coinManager();
+}
+
+function render(raf) {
+    if (jumping)
+        boy.className = "boyJump";
+    else
+        boy.className = "boyRun";
+    let txtTime = Math.round(remainingTime / 60);
+    let txtScore = coins;
+    time.innerHTML = txtTime;
+    score.innerHTML = txtScore;
+    if(finish) finishGame(raf);
+}
+
+function gameLoop() {
+    raf = window.requestAnimationFrame(gameLoop);
+    now = timestamp();
+    dt = (now - last) / 1000;
+    update();
+    render(raf);
+    last = now;
+}
+
+function startGame() {
+    remainingTime = gameTime * 60;
+    coins = 0;
+    document.addEventListener("keydown", handleKeyDown);
+    startView.className = "hidden";
+    setBack();
+    finish = false;
+    raf = window.requestAnimationFrame(gameLoop);
+}
+
+function setBack() {
+    let randomBack = Math.floor(Math.random() * 10) ;
+    if (randomBack < 5)
+        back.className = "back-3";
+    else
+        back.className = "back-2";
+}
+
+function finishGame(raf) {
+    cancelAnimationFrame(raf);
+    document.removeEventListener("keydown", handleKeyDown);
+    startView.className = "startView";
+    let gameOver = document.getElementById("gameOver");
+    let coinsColleted = document.getElementById("coinsCollect");
+    gameOver.innerText = "GAME OVER";
+    coinsColleted.innerText = "Total de monedas: " + coins;
+    back.className = "hidden";
+}
+
+function handleKeyDown(e) {
+    if (e.keyCode == 38) {
+        jumping = true;
+        startJump = timestamp();
+    }
+}
+
+function detectarColision() {
+    let a_pos = 
+            {   t: boy.getBoundingClientRect().top, 
+                l: boy.getBoundingClientRect().left, 
+                r: boy.getBoundingClientRect().left + boy.getBoundingClientRect().width, 
+                b: boy.getBoundingClientRect().top + boy.getBoundingClientRect().height,
+            };
+    let b_pos = 
+        {   t: coin.getBoundingClientRect().top, 
+            l: coin.getBoundingClientRect().left, 
+            r: coin.getBoundingClientRect().left + coin.getBoundingClientRect().width, 
+            b: coin.getBoundingClientRect().top + coin.getBoundingClientRect().height,
+        };
+    if(a_pos.l <= b_pos.r && a_pos.r >= b_pos.l 
+        && a_pos.b >= b_pos.t && a_pos.t <= b_pos.b ) {
+            colision();
+        }
+}
+
+function playSound() {
+    let audio = {};
+    audio["bien"] = new Audio();
+    audio["bien"].src = "js/sound/buenaViejo.mp4";
+    audio["bien"].play();
+}
+
+
+function colision() {
+    coins += 1;
+    remainingTime = remainingTime + 4 * 60;
+    playSound();
+    coin.className = "hidden";
+    coinContainer.className = "hidden";
+    coinManegerTime = timestamp();
+}
+
+function coinManager() {
+    if (coinManegerTime + 4000 < timestamp() && coin.className == "hidden") {
+        coin.className = "coin";
+        coinContainer.className = "coin-container";
+    }
+
+}
+
+function timestamp() {
+    return window.performance && window.performance.now ? window.performance.now() : new Date().getTime();
+}
+
+// La idea es que esta función te deribe a la pagina que te genero el error 404
+
+function handleExitGame() { location.href = "https://www.google.com/"; }
